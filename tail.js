06b00e1c7ef75d0af572ecf3c1fd4d29b089c27c
valik94 <valentin.lyashenko@ryerson.ca>
2021-10-30 15:55:54 +0000

    commiting without.js to git a function that takes in 2 arrays and the source array and array to remove from. and returns a new array based on the subtraction of element itemstoRemove from the source array. also includes assertArrayEquals for the console.log to show the

---
+++ tail.js
@@ -0,0 +1,33 @@
+function tail(arr1) {
+  let newarr=[];
+  if (arr1.length === 0) {
+    return newarr;
+  }
+  if (arr1.length === 1) {
+    return newarr;
+  }
+  console.log(arr1);
+  newarr=arr1.slice(1);
+  return newarr;
+}
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected);
+  if (actual === expected) {
+    console.log(`Assertion Passed: %s  === %s `, actual, expected);
+  } else {
+    console.log(`Assertion Failed: %s !== %s `, actual, expected);
+  }
+};
+
+// TEST CODE
+//TEST CASE 1
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); 
+
+//TEST CASE
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
---
+++ without.js
@@ -0,0 +1,43 @@
+const assertArraysEqual = function (array1, array2){
+  const arraysAreEqual = eqArrays(array1, array2);
+  if (arraysAreEqual) {
+    console.log(`Assertion Passed: ${array1}  === ${array2} `);
+  }
+  else {
+    console.log(`Assertion Failed: ${array1}  !== ${array2} `);
+  }
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  for (let i=0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const without = function (source, itemsToRemove) {
+ let resultarr=[];
+ //console.log(itemsToRemove)
+ for (let i=0; i < source.length; i++){
+   if (source[i] != itemsToRemove){
+    resultarr.push(source[i]);
+   }
+   else{
+    continue;
+   }
+   //console.log(source[i])
+ }
+ console.log(resultarr);
+ return resultarr;
+}
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(words, [ "lighthouse"]);
