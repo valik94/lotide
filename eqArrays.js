1c1c99e372fcc13bc12ab008c008967e6af261ee
valik94 <valentin.lyashenko@ryerson.ca>
2021-11-02 02:06:53 +0000

    refactored eqArray.js file for modularity and included in tests folder eqArraysTest.js that tests this function and imports from it

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,13 @@
-const assertEqual = function(actual, expected) {
-  //console.assert(actual === expected);
-  if (actual === expected) {
-    console.log(`Assertion Passed: %s  === %s `, actual, expected);
-  } else {
-    console.log(`Assertion Failed: %s !== %s `, actual, expected);
-  }
-};
+const assertEqual = require('./assertEqual');
+
+// const assertEqual = function(actual, expected) {
+//   //console.assert(actual === expected);
+//   if (actual === expected) {
+//     console.log(`Assertion Passed: %s  === %s `, actual, expected);
+//   } else {
+//     console.log(`Assertion Failed: %s !== %s `, actual, expected);
+//   }
+// };
 
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length){
@@ -18,10 +20,14 @@ const eqArrays = function (arr1, arr2) {
   }
   return true;
 }
-assertEqual(eqArrays([1, 2], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+// //TEST CASES
+// assertEqual(eqArrays([1, 2], [1, 2, 3]), true); // => true
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+module.exports = eqArrays;
